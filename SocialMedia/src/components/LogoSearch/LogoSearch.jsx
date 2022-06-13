751b8bf74d6e2dd88e4706060ba3d17bc2477fb1
phubinh01022001 <59392168+phubinh01022001@users.noreply.github.com>
2022-06-13 17:30:18 +0700

    first commit

---
+++ SocialMedia/src/components/LogoSearch/LogoSearch.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Logo from '../../img/logo2.png'
+import {UilSearch} from '@iconscout/react-unicons'
+import './LogoSearch.css'
+const LogoSearch = () => {
+  return (
+   <div className="LogoSearch">
+       <img id='logo' src={Logo} alt="" />
+       <div className="Search">
+           <input type="text" placeholder='#Explore' />
+           <div className="s-icon">
+               <UilSearch/>
+           </div>
+       </div>
+   </div>
+  )
+}
+
+export default LogoSearch
