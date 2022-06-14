f9e41a1607fd010a7fe527b88883c2b7bf1de16a
phubinh01022001 <59392168+phubinh01022001@users.noreply.github.com>
2022-06-14 10:44:12 +0700

    add new

---
+++ SocialMedia/src/Data/FollowersData.js
@@ -0,0 +1,11 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  { name: "Be Bun", username: "AnNhien", img: img1 },
+  { name: "Kim Binh", username: "BeBinh", img: img2 },
+  { name: "Huy Binh", username: "HuyBinh", img: img3 },
+  { name: "CamLinh", username: "CamLinh1702", img: img4 },
+];
---
+++ SocialMedia/src/Data/TrendData.js
@@ -0,0 +1,27 @@
+export const TrendData= [
+    {
+      name: "Avengers",
+      shares: 97,
+    },
+    {
+      name: "Avatar2",
+      shares: 80.5,
+    },
+    {
+      name: "Ukraine",
+      shares: 75.5,
+    },
+    {
+      name: "Reactjs",
+      shares: 72,
+    },
+    {
+      name: "Facebook",
+      shares: 71.9,
+    },
+    {
+      name: "Youtube",
+      shares: 20,
+    },
+  ];
+  
